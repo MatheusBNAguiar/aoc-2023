a1bcedffffd788ecc1be938cbe28b2eed542212e
Matheus <matheus.bnaguiar@gmail.com>
2023-12-05 10:47:40 -0300

    chore: Rewrite code to improve readability

--- day-3/day-3.mjs
+++ day-3/day-3.mjs
@@ -1,24 +1,26 @@
 import { getDayInput } from "../utils/getDayInput.mjs";
 
-export default function getAnswer() {
+function getReferences() {
   const fileResult = getDayInput(import.meta)
   const baseArray = fileResult.split('\n');
   const matrixArray = baseArray.map(i => i.split(''));
+  return { baseArray, matrixArray };
+}
 
+function getPart1Answer({ baseArray, matrixArray }) {
   let baseSum = 0;
-
   for (let arrayLine = 0; arrayLine < baseArray.length; arrayLine++) {
     let lineValue = baseArray[arrayLine]
     lineValue.match(/\d+/gm)?.forEach(numberOnLine => {
       const numberStart = lineValue.indexOf(numberOnLine);
 
-      let numberFound = false;
       const leftBoundary = Math.max(numberStart - 1, 0);
       const rightBoundary = numberStart + numberOnLine.length;
 
       const topBoundary = Math.max(arrayLine - 1, 0);
       const bottomBoundary = arrayLine + 1;
 
+      let numberFound = false;
       for (let squareY = topBoundary; squareY <= bottomBoundary && !numberFound; squareY++) {
         for (let squareX = leftBoundary; squareX <= rightBoundary && !numberFound; squareX++) {
           const selectedDigit = matrixArray?.[squareY]?.[squareX];
@@ -34,4 +36,11 @@ export default function getAnswer() {
   }
 
   return baseSum;
+}
+
+export default function getAnswer() {
+  const { baseArray, matrixArray } = getReferences();
+  const part1Result = getPart1Answer({ baseArray, matrixArray });
+
+  return `Part 1: ${part1Result} Part 2: ${part1Result}`;
 }
