7a19dcbdfb57fd587c077ed1d9e358af23b2e217
Matheus <matheus.bnaguiar@gmail.com>
2023-12-05 12:10:29 -0300

    chore: Uncomment answers

--- all-responses.mjs
+++ all-responses.mjs
@@ -1,11 +1,11 @@
-// import getDay1Answer from "./day-1/day-1.mjs";
-// console.log(`Day 1 Answer: ${getDay1Answer()}`)
+import getDay1Answer from "./day-1/day-1.mjs";
+console.log(`Day 1 Answer: ${getDay1Answer()}`)
 
-// import getDay2Answer from "./day-2/day-2.mjs";
-// console.log(`Day 2 Answer: ${getDay2Answer(12, 13, 14)}`)
+import getDay2Answer from "./day-2/day-2.mjs";
+console.log(`Day 2 Answer: ${getDay2Answer(12, 13, 14)}`)
 
 import getDay3Answer from "./day-3/day-3.mjs";
 console.log(`Day 3 Answer: ${getDay3Answer()}`)
 
-// import getDay4Answer from "./day-4/day-4.mjs";
-// console.log(`Day 4 Answer: ${getDay4Answer()}`)
+import getDay4Answer from "./day-4/day-4.mjs";
+console.log(`Day 4 Answer: ${getDay4Answer()}`)
