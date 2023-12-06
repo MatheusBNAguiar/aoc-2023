fdd4f23ecd838a5362364f9f708624e4f8ff071f
Matheus <matheus.bnaguiar@gmail.com>
2023-12-06 19:05:53 -0300

    Update scaffold

--- day-x/day-x.mjs
+++ day-x/day-x.mjs
@@ -1,5 +1,11 @@
 import { getDayInput } from "../utils/getDayInput.mjs";
 
+function getPart1Answer() { }
+
+function getPart2Answer() { }
+
 export default function getAnswer() {
   const fileResult = getDayInput(import.meta)
+
+  return `Part 1: ${getPart1Answer(entries)} Part 2: ${getPart2Answer(entries)}`
 }
